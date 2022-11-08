b75f20d5f7bb168189a5767ad02a2aeb857d199b
Prajwal Mohite <prajwalmohite1@gmail.com>
2022-11-08 15:11:34 +0530

    project added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { AboutusComponent } from './components/aboutus/aboutus.component';
+import { PrivacypolicyComponent } from './components/privacypolicy/privacypolicy.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    AboutusComponent,
+    PrivacypolicyComponent
   ],
   imports: [
     BrowserModule,
